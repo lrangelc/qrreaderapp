ef9042662f114605c12f73258c928fe4f101772e
Luis Rangel <luisrangelc@gmail.com>
2020-03-30 23:37:37 -0600

    check estado en home_page.dart currentIndex

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'package:qrreaderapp/src/pages/direcciones_page.dart';
+import 'package:qrreaderapp/src/pages/mapas_page.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _callPage(currentIndex),
+      bottomNavigationBar: _crearBottomNavigationBar(),
+    );
+  }
+
+  Widget _callPage(int paginaActual) {
+    switch (paginaActual) {
+      case 0:
+        return MapasPage();
+
+      case 1:
+        return DireccionesPage();
+
+      default:
+        return MapasPage();
+    }
+  }
+
+  Widget _crearBottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index) {
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.map), title: Text('Mapas')),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.brightness_5), title: Text('Direcciones')),
+      ],
+    );
+  }
+}
---
+++ lib/src/pages/mapas_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MapasPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Mapa Page'),
+      ),
+    );
+  }
+}
